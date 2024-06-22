df4bfc5a68b77ecde82243208b9b83b3d55587a8
Nick <103455498+nickjolshansky@users.noreply.github.com>
2024-06-22 15:34:16 -0400

    Delete t.py

--- t.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-
-# Get a list of all the open windows
-all_windows = pyautogui.getWindowsWithTitle('')
-
-# Print the title of each window
-for window in all_windows:
-    print(window.title)
